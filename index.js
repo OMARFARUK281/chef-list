bb037f4dd296667d6fcf3b2fd33cc043b0ea7930
OmarFaruk <omarfaruk8431@gmail.com>
2023-05-05 21:31:50 +0600

    added user

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chef = require('./ChefFood/chef.json');
+const foods = require('./ChefFood/foods.json');
 
 app.use(cors());
 
@@ -16,20 +17,32 @@ app.get('/chef', (req, res) =>{
 })
 
 
-
-/* const foods = require('./ChefFood/foods.json');
-
 app.get('/foods', (req, res) =>{
+    res.send(foods);
+})
+
+app.get('/foods', (req, res) =>{    
     res.send(foods);
 })
 
 app.get('/foods/:id', (req, res) =>{
     const id = req.params.id;
     console.log(id);
-    const selectedFoods = foods.find(n = n.id === id);
-    res.send(selectedFoods)
-}) */
+    const selectedFoods = foods.find(n => n.id === id);
+    res.send(selectedFoods); 
+})
 
+
+app.get('/chef/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    if(id === 0){
+        res.send(foods)
+    }
+    else{
+        const  chefFoods = foods.filter(n => parseInt(n.id) === id);
+        res.send(chefFoods)
+    }
+})
 
 
 app.listen(port, () =>{
